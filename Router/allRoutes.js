bafc69719274fcfebb48cf47548ab8bc732ef736
Bourdeloux Corinne <bourdeloux.corinne@orange.fr>
2024-10-02 13:25:21 +0200

    ajout images

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,13 +3,7 @@ import Route from "./Route.js";
 // Définir vos routes ici
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/pages/connexion", "connexion", "/pages/connexion.html"),
-    new Route("/pages/contact", "Contact", "/pages/contact.html"),
-    new Route("/pages/habitats", "Habitats", "/pages/habitats.html"),
-    new Route("/pages/jungle", "Jungle", "/pages/jungle.html"),
-    new Route("/pages/marais", "Marais", "/pages/marais.html"),
-    new Route("/pages/savane", "Savane", "/pages/savane.html"),
-    new Route("/pages/service", "Service", "/pages/services.html"),
+    
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
