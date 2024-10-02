6b837f4fe812feb65e6b82c68087e4ba7b052e30
Bourdeloux Corinne <bourdeloux.corinne@orange.fr>
2024-10-02 09:50:13 +0200

    validation supression fichiers inutiles

--- Router/allRoutes.js
+++ Router/allRoutes.js
@@ -3,13 +3,13 @@ import Route from "./Route.js";
 // Définir vos routes ici
 export const allRoutes = [
     new Route("/", "Accueil", "/pages/home.html"),
-    new Route("/connexion", "connexion", "/pages/connexion.html"),
-    new Route("/contact", "Contact", "/pages/contact.html"),
-    new Route("/habitats", "Habitats", "/pages/habitats.html"),
-    new Route("/jungle", "Jungle", "/pages/jungle.html"),
-    new Route("/marais", "Marais", "/pages/marais.html"),
-    new Route("/savane", "Savane", "/pages/savane.html"),
-    new Route("/service", "Service", "/pages/services.html"),
+    new Route("/pages/connexion", "connexion", "/pages/connexion.html"),
+    new Route("/pages/contact", "Contact", "/pages/contact.html"),
+    new Route("/pages/habitats", "Habitats", "/pages/habitats.html"),
+    new Route("/pages/jungle", "Jungle", "/pages/jungle.html"),
+    new Route("/pages/marais", "Marais", "/pages/marais.html"),
+    new Route("/pages/savane", "Savane", "/pages/savane.html"),
+    new Route("/pages/service", "Service", "/pages/services.html"),
 ];
 
 // Le titre s'affiche comme ceci : Route.titre - websitename
